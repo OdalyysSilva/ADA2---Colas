e0ba399ec96dfbd913c3636e952c46c10ac9042a
Odalys Silva Colli <margely2604@hotmail.com>
2024-10-15 18:12:53 -0600

    Update cola1.py

--- cola1.py
+++ cola1.py
@@ -12,8 +12,7 @@ def suma(cola1, cola2):
     print("\nCola 1   Cola 2   Suma de la cola 1 y 2")
     while cola1.valor and cola2.valor:
         elemento1 = cola1.desencolar()
-        elemento2 = cola2.desencolar()
-         # Suma los elementos desencolados
+        elemento2 = cola2.desencolar() # Suma los elementos desencolados
         cola3 = elemento1 + elemento2
         print(f"  {elemento1:<8} {elemento2:<9} {cola3:<5}\n")
 
@@ -22,7 +21,6 @@ def definir(cola, nomcola, n):
         cola.encolar(int(input(f"Inserta un número para la {nomcola}: ")))
 
 n = int(input("\nIserta el numero de elementos para las cola: "))
-
 # Crea dos instancias de la clase Cola (cola1 y cola2)
 cola1, cola2 = Cola(), Cola()
 definir(cola1, "Cola 1", n)
